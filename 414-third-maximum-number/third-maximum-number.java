70bef3c66009542111b62bda55e37fa092632d12
subamathiR <subamathi881@gmail.com>
2026-02-10 16:15:59 +0530

    Time: 6 ms (44.04%) | Memory: 46.9 MB (6.39%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int num : nums) {
+            set.add(num);
+            if (set.size() > 3) {
+                set.pollFirst(); 
+            }
+        }
+        return set.size() == 3 ? set.first() : set.last();
+    }
+}
