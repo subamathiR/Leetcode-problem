c1beb32c55e0a8bbfec96a271ecccd7662f8b8a7
subamathiR <subamathi881@gmail.com>
2026-03-03 20:52:48 +0530

    Time: 13 ms (11.46%) | Memory: 52.7 MB (5.03%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+        Set<String> set = new HashSet<>();
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                
+                char num = board[i][j];
+                
+                if (num != '.') {
+                    
+                    if (!set.add(num + " in row " + i) ||
+                        !set.add(num + " in column " + j) ||
+                        !set.add(num + " in block " + i/3 + "-" + j/3)) {
+                        
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+}
