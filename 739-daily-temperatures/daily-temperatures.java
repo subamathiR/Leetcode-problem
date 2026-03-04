872b8a9493ff0a73eb7e64eb4894a4a57d2abed2
subamathiR <subamathi881@gmail.com>
2026-03-04 19:35:38 +0530

    Time: 60 ms (77.29%) | Memory: 107.5 MB (51.63%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[temperatures.length];
+        
+        for(int i = 0; i < temperatures.length; i++) {
+            
+            while(!stack.isEmpty() &&  temperatures[i] > temperatures[stack.peek()]) {
+                
+                int last = stack.pop();
+                ans[last] = i - last;
+            }
+            
+            stack.push(i);
+        }
+        
+        return ans;
+    }
+}
