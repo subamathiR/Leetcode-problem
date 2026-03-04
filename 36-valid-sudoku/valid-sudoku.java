0745c83e655c3df21564a321e4595d622384ed25
subamathiR <subamathi881@gmail.com>
2026-03-04 18:47:24 +0530

    Time: 2 ms (65.70%) | Memory: 46.2 MB (96.04%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,25 +1,28 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         
-        Set<String> set = new HashSet<>();
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
         
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
                 
-                char num = board[i][j];
-                
-                if (num != '.') {
-                    
-                    if (!set.add(num + " in row " + i) ||
-                        !set.add(num + " in column " + j) ||
-                        !set.add(num + " in block " + i/3 + "-" + j/3)) {
-                        
-                        return false;
-                    }
+                if(board[i][j] == '.'){
+                    continue;   // skip empty cells
                 }
+                
+                int num = board[i][j] - '1';   // convert char to index (0-8)
+                
+                int boxIndex = (i/3) * 3 + (j/3);
+                
+                if(row[i][num] || col[j][num] || box[boxIndex][num]){
+                    return false;  // duplicate found
+                }
+                
+                row[i][num] = true;
+                col[j][num] = true;
+                box[boxIndex][num] = true;
             }
         }
         
