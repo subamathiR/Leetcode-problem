050acf40bfa9b4294cafca1db6d9334635e5552e
subamathiR <subamathi881@gmail.com>
2026-03-07 09:55:06 +0530

    Time: 1 ms (98.40%) | Memory: 43.3 MB (96.47%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for(int i = 0; i < num.length(); i++){
+            int d = num.charAt(i) - '0';
+
+            if(i % 2 == 0){
+                evenSum += d;
+            }
+            else{
+                oddSum += d;
+            }
+        }
+
+        return evenSum == oddSum;
+    }
+}
