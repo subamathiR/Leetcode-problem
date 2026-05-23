c43555c08dc1961405db399c2f72f253d4c01a92
subamathiR <subamathi881@gmail.com>
2026-05-23 09:59:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (50.43%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int c = 0;
+        for(int i=s.length()-1;i>=0;i--){
+            char ch = s.charAt(i);
+            if(ch!=' '){
+                c++;
+            }
+            else{
+                break;
+            }
+        }
+        return c;
+    }
+}
