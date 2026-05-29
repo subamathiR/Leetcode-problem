f7460cb2b0372e361c6adcc12781c8a1d8e338dd
subamathiR <subamathi881@gmail.com>
2026-05-29 14:46:19 +0530

    Time: 2 ms (99.90%) | Memory: 45.3 MB (90.03%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,34 +1,50 @@
 class Solution {
+
     public String minWindow(String s, String t) {
-        int maps[] = new int[256];
-        int mapt[] = new int[256];
+
+        int freq[] = new int[256];
+
         for(char ch : t.toCharArray()) {
-            mapt[ch]++;
+            freq[ch]++;
         }
+
         int left = 0;
-        int min = Integer.MAX_VALUE;
-        int minstart = 0;
+        int count = t.length();
+
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
         for(int right = 0; right < s.length(); right++) {
-            maps[s.charAt(right)]++;
-            while(contains(maps, mapt)) {
-             if(right - left + 1 < min) {
-                    min = right - left + 1;
-                    minstart = left;
+
+            char ch = s.charAt(right);
+
+            if(freq[ch] > 0) {
+                count--;
+            }
+
+            freq[ch]--;
+
+            while(count == 0) {
+
+                if(right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
                 }
-                maps[s.charAt(left)]--;
+
+                char leftChar = s.charAt(left);
+
+                freq[leftChar]++;
+
+                if(freq[leftChar] > 0) {
+                    count++;
+                }
+
                 left++;
             }
         }
-        return min == Integer.MAX_VALUE
+
+        return minLen == Integer.MAX_VALUE
                 ? ""
-                : s.substring(minstart, minstart + min);
-    }
-    public boolean contains(int maps[], int mapt[]) {
-        for(int i = 0; i < 256; i++) {
-            if(mapt[i] > maps[i]) {
-                return false;
-            }
-        }
-        return true;
+                : s.substring(start, start + minLen);
     }
 }
