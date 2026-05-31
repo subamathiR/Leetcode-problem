02a62dc2cde8c2b22c2c59cc0e4c613877f9412a
subamathiR <subamathi881@gmail.com>
2026-05-31 18:21:42 +0530

    Time: 1 ms (89.13%) | Memory: 43.1 MB (78.22%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(check(word,'A','Z') || 
+           check(word,'a','z') || 
+           check(word.substring(1),'a','z')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static boolean check(String word,char start,char end){
+         for(char ch : word.toCharArray()){
+            if(ch < start || ch > end){
+                return false;
+            }
+         }
+         return true;
+    }
+}
