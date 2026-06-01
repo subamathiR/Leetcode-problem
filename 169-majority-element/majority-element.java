4148b5a730eae9fffcc5e795ca5a66e2091f9cb3
subamathiR <subamathi881@gmail.com>
2026-06-01 17:53:31 +0530

    Time: 13 ms (29.51%) | Memory: 52.6 MB (85.39%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,11 +3,12 @@ class Solution {
         Map<Integer,Integer> map = new HashMap<>();
         for(int num:nums){
             map.put(num,map.getOrDefault(num,0)+1);
-            if(map.get(num)>nums.length/2){
-            return num;
         }
+        for(Map.Entry<Integer,Integer> e:map.entrySet()){
+            if(e.getValue()>nums.length/2){
+                return e.getKey();
+            }
         }
-        
         return -1;
     }
 }
