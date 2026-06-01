f61bd5e68f8ef092d6e0ed86969718cfd050b987
subamathiR <subamathi881@gmail.com>
2026-06-01 14:57:08 +0530

    Time: 2 ms (60.80%) | Memory: 43.4 MB (67.34%) - LeetSync

---
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int secondHighest(String s) {
+        int first = -1;
+        int second = -1;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)){
+                int d = ch-'0';
+                if(d > first){
+                    second = first;
+                    first = d;
+                }
+                else if(d < first && d > second){
+                     second = d;
+                }
+            }
+        }
+        return second;
+    }
+}
