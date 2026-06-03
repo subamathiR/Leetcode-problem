bff0a97b20d1c0dbc1dff679a4220f1fe2f32900
subamathiR <subamathi881@gmail.com>
2026-06-03 14:38:00 +0530

    Time: 1 ms (96.99%) | Memory: 42.1 MB (84.52%) - LeetSync

---
+++ 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int largestInteger(int num) {
+        char ch[] = String.valueOf(num).toCharArray();
+        int f[] = new int[10];
+        for(char c:ch){
+            f[c-'0']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char c:ch){
+            int digit = c-'0';
+            if(digit%2==0){
+                for(int d = 8;d>=0;d-=2){
+                    if(f[d]>0){
+                    sb.append(d);
+                    f[d]--;
+                    break;
+                }}
+            }
+            else{
+                for(int d = 9;d>=1;d-=2){
+                    if(f[d]>0){
+                    sb.append(d);
+                    f[d]--;
+                    break;
+                }}
+            }
+        }
+        return Integer.parseInt(sb.toString());
+    }
+}
