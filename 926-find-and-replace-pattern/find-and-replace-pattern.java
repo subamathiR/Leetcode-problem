6c5848dad14000319507acd2a82c32168da9c5ed
subamathiR <subamathi881@gmail.com>
2026-06-18 12:28:23 +0530

    Time: 1 ms (77.93%) | Memory: 43.7 MB (79.50%) - LeetSync

---
+++ 926-find-and-replace-pattern/find-and-replace-pattern.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> result = new ArrayList<>();
+        for(String word:words){
+            if(check(word,pattern)){
+                result.add(word);
+            }
+        }
+        return result;
+    }
+    public boolean check(String word,String pattern){
+        Map<Character,Character> map = new HashMap<>();
+        for(int i=0;i<word.length();i++){
+            char k = word.charAt(i);
+            char v = pattern.charAt(i);
+            if(map.containsKey(k)){
+                if(map.get(k)!=v){
+                    return false;
+                }
+            }
+            else{
+                if(map.containsValue(v)){
+                    return false;
+                }
+                map.put(k,v);
+            }
+        }
+        return true;
+    }
+}
