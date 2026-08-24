27930c871a1ae705596e72b7883f37a8f33534df
subamathiR <subamathi881@gmail.com>
2026-08-24 12:19:30 +0530

    Time: 391 ms (88.26%) | Memory: 46.6 MB (95.96%) - LeetSync

---
+++ 4318-valid-subarrays-with-matching-sum-digits-i/valid-subarrays-with-matching-sum-digits-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countValidSubarrays(int[] nums, int x) {
+        int n = nums.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            long sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum % 10 == x) {
+                    long temp = sum;
+                    while (temp >= 10) {
+                        temp /= 10;
+                    }
+                    if (temp == x) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
