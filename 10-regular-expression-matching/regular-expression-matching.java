f174863f7945d7ac982efb9844944daee6499b67
subamathiR <subamathi881@gmail.com>
2026-09-08 10:24:10 +0530

    Time: 460 ms (8.46%) | Memory: 44.1 MB (8.68%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        s = s.replace(".","").replace("*",".");
-        if(s.matches(p))
-        return true;
-        else
-        return false;
+      return s.matches(p);
     }
 }
