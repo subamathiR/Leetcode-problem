db7db4f5d7c105b44a9c2c572cc3dd9ab8043bc9
subamathiR <subamathi881@gmail.com>
2026-09-08 11:24:16 +0530

    Time: 1 ms (99.87%) | Memory: 46.5 MB (81.20%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for(int i = 0;i<n;i++){
+            Arrays.fill(board[i],'.');
+        }
+        boolean cols[] = new boolean[n];
+        boolean diag1[] = new boolean[2*n-1];
+        boolean diag2[] = new boolean[2*n-1];
+        backtrack(0,n,board,cols,diag1,diag2,result);
+        return result;
+    }
+    public void backtrack(int row,int n,char board[][],boolean cols[],boolean diag1[],boolean diag2[],List<List<String>> result){
+        if(row == n){
+            List<String> list = new ArrayList<>();
+            for(char[] r : board){
+                list.add(new String(r));
+            }
+            result.add(list);
+            return;
+        }
+        for(int col = 0;col < n;col++){
+            int d1 = row-col+n-1;
+            int d2 = row+col;
+            if(cols[col] || diag1[d1] || diag2[d2]){
+                continue;
+            }
+            board[row][col] = 'Q';
+            cols[col] = true;
+            diag1[d1] = true;
+            diag2[d2] = true;
+            backtrack(row + 1,n,board,cols,diag1,diag2,result);
+            board[row][col] = '.';
+            cols[col] = false;
+            diag1[d1] = false;
+            diag2[d2] = false;
+        }
+    }
+}
